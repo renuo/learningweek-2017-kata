a858a55f990f10c6d80a8f6614952b73fcf6519c
rbuetzer <richard.buetzer@renuo.ch>
2017-12-01 13:45:13 +0100

    Initial commit

---
+++ lib/substring_puzzle.rb
@@ -0,0 +1,5 @@
+module SubstringPuzzle
+  def self.substring_puzzle(string, substrings)
+    # TODO
+  end
+end
---
+++ spec/substring_puzzle_spec.rb
@@ -0,0 +1,52 @@
+require 'rspec-benchmark'
+require 'substring_puzzle'
+
+describe SubstringPuzzle do
+  include RSpec::Benchmark::Matchers
+
+  # Credits: https://codegolf.stackexchange.com/questions/117872/test-if-a-string-can-be-made-with-substrings
+  describe 'substring_puzzle' do
+    subject { SubstringPuzzle.substring_puzzle(string, substrings)}
+
+    context 'when some substrings are used twice' do
+      let(:string) { 'Hello, world!' }
+      let(:substrings) { ['l', 'He', 'o, wor', 'd!'] }
+      it { is_expected.to be true }
+    end
+
+    context 'when no substrings match the string' do
+      let(:string) { 'this is a string' }
+      let(:substrings) { ['these ', 'substrings ', 'do ', 'not ', 'match'] }
+      it { is_expected.to be false }
+    end
+
+    context 'when some substrings do not match' do
+      let(:string) { 'this is a string' }
+      let(:substrings) { ['this ', 'was ', 'a ', 'string'] }
+      it { is_expected.to be false }
+    end
+
+    context 'with unused substrings' do
+      let(:string) { 'Happy coding!' }
+      let(:substrings) { ['coding', '!', 'Happy', ' ', 'spam'] }
+      it { is_expected.to be true }
+    end
+
+    context 'with an empty string' do
+      let(:string) { '' }
+      let(:substrings) { ['The empty string can be constructed with nothing'] }
+      it { is_expected.to be true }
+    end
+
+    # Only for the brave
+    xdescribe 'performance' do
+      context 'with large amount of possible combinations' do
+        let(:string) { 'ababababababababababababaX' }
+        let(:substrings) { %w[a b ab ba] }
+        it 'performs well' do
+          expect { subject }.to perform_under(5).ms.and_sample(5)
+        end
+      end
+    end
+  end
+end
