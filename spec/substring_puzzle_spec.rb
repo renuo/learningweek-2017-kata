bb8057a85cb95fa00f2979a66ddc1d49e338d932
rbuetzer <richard.buetzer@renuo.ch>
2017-12-03 16:36:52 +0100

    Add missing assertion

--- spec/substring_puzzle_spec.rb
+++ spec/substring_puzzle_spec.rb
@@ -53,6 +53,7 @@ describe SubstringPuzzle do
     context 'when the input is not suitable for greedy algorithms' do
       let(:string) { 'ababab' }
       let(:substrings) { %w(a ba ab) }
+      it { is_expected.to be true }
     end
 
     # Only for the brave
