a59d8e6cac45f6c23d6f179b4911d50adc3bb4d8
Felix Kubli <felix.kubli@renuo.ch>
2017-12-03 16:38:05 +0100

    version 1

--- lib/substring_puzzle.rb
+++ lib/substring_puzzle.rb
@@ -1,5 +1,14 @@
 module SubstringPuzzle
   def self.solvable?(string, substrings)
-    # TODO
+    result = ''
+    i = 0
+    while i < substrings.size
+      i += 1
+      if string.start_with?(result + substrings[i - 1])
+        result += substrings[i - 1]
+        i = 0
+      end
+    end
+    result == string
   end
 end
