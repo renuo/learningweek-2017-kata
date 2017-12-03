cfc30f602d8d8670341227c650dec4f1fbce9c78
Sebastian Rothmund <sebastian.rothmund@renuo.ch>
2017-12-03 17:52:55 +0100

    ugly but works

--- lib/substring_puzzle.rb
+++ lib/substring_puzzle.rb
@@ -1,5 +1,20 @@
 module SubstringPuzzle
   def self.solvable?(string, substrings)
-    # TODO
+    substrings = substrings.sort
+    tempstring1 = string.dup
+    substrings.each do |substring|
+      if tempstring1.include? substring
+        tempstring1.gsub! substring, ''
+      end
+    end
+    substrings = substrings.sort.reverse
+    tempstring2 = string.dup
+    substrings.each do |substring|
+      if tempstring2.include? substring
+        tempstring2.gsub! substring, ''
+      end
+    end
+    return false if tempstring1 != '' && tempstring2 != ''
+    return true
   end
 end
