22f0bde5deaa9ff407a792db600fa9f10bda80d8
rbuetzer <richard.buetzer@renuo.ch>
2017-12-06 18:05:50 +0100

    Add solution for square kata

--- lib/square.rb
+++ lib/square.rb
@@ -2,4 +2,11 @@ module Square
   def self.draw(n)
     # TODO
   end
+
+  def self.draw_solution(n)
+    k = n-2
+    ('+' + '-'*k + '+' + "\n") +
+        ('|' + ' '*k + '|' + "\n") * k +
+        ('+' + '-'*k + '+')
+  end
 end
